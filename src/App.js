7fede77198939e00fe0ff05902d33e81d8a01b86
Nickolay Kondratyev <trase.nick@yandex.ru>
2022-04-18 15:42:30 +0300

    Add header menu

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
 		setAccess(false);
 	};
 	return (
-		<div>
+		<div className="wrapper">
 			{accessGranted ? (
 				<MainPage logout={logout} />
 			) : (
--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -6,10 +6,14 @@ import {
 	LeftOutlined,
 	RightOutlined,
 	EditOutlined,
+	CommentOutlined,
+	SettingOutlined,
+	LogoutOutlined,
 	OrderedListOutlined,
 } from "@ant-design/icons";
 
 const { Header, Sider, Content } = Layout;
+const { SubMenu } = Menu;
 
 const menuItems = [
 	{
@@ -33,52 +37,46 @@ const getMenuIcon = (value) => {
 	}
 };
 
-const MainPage = () => {
+const MainPage = (props) => {
 	const [menuOpened, setMenuOpen] = useState(false);
 	const [activeMenuItem, setActiveMenuItem] = useState("request-list");
 	const [contentTitle, setContentTitle] = useState("Список заявок");
 
-	const setMenuTitle = (item) => {
-		let title = "";
-		if (item === "new-request") {
-			title = "Поле новой заявки";
-		}
-		if (item === "request-list") {
-			title = "Список заявок";
-		}
-		setActiveMenuItem(item);
-		setContentTitle(title);
+	const openMenu = () => {
+		setMenuOpen(true);
+	};
+
+	const closeMenu = () => {
+		setMenuOpen(false);
 	};
 
 	const setContent = () => {
 		// ! TODO: get content using axios
 		if (activeMenuItem === "new-request") {
-			console.log("new-request");
 			return <h2>Поле новой заявки</h2>;
 		}
 		if (activeMenuItem === "request-list") {
-			console.log("request-list");
 			return <h2>Список заявок</h2>;
 		}
 	};
 
 	return (
-		<Layout>
-			<Sider trigger={null} collapsible collapsed={menuOpened}>
-				{React.createElement(
-					this.state.collapsed ? RightOutlined : LeftOutlined,
-					{
-						className: "trigger",
-						onClick: setMenuOpen(!menuOpened),
-					}
+		<Layout className="page">
+			<Sider trigger={null} collapsible collapsed={!menuOpened}>
+				{menuOpened ? (
+					<LeftOutlined className="trigger" onClick={closeMenu} />
+				) : (
+					<RightOutlined className="trigger" onClick={openMenu} />
 				)}
-
 				<Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
 					{menuItems.map((item) => (
 						<Menu.Item
 							key={item.id}
 							icon={getMenuIcon(item.value)}
-							onClick={() => setMenuTitle(item.value)}
+							onClick={() => {
+								setActiveMenuItem(item.value);
+								setContentTitle(item.text);
+							}}
 						>
 							{item.text}
 						</Menu.Item>
@@ -90,7 +88,50 @@ const MainPage = () => {
 					className="site-layout-background"
 					style={{ padding: 0 }}
 				>
-					<h2 className="content-title">{contentTitle}</h2>
+					<div className="header">
+						<div className="header-title">{contentTitle}</div>
+						<div className="header-buttons">
+							<Menu mode="horizontal" selectedKeys={[]}>
+								<Menu.Item
+									key="button"
+									icon={<CommentOutlined />}
+								>
+									Button
+								</Menu.Item>
+								<SubMenu
+									key="submenu"
+									icon={<SettingOutlined />}
+									title="Submenu"
+								>
+									<Menu.ItemGroup title="Item 1">
+										<Menu.Item key="setting:1">
+											Option 1
+										</Menu.Item>
+										<Menu.Item key="setting:2">
+											Option 2
+										</Menu.Item>
+									</Menu.ItemGroup>
+									<Menu.ItemGroup title="Item 2">
+										<Menu.Item key="setting:3">
+											Option 3
+										</Menu.Item>
+										<Menu.Item key="setting:4">
+											Option 4
+										</Menu.Item>
+									</Menu.ItemGroup>
+								</SubMenu>
+								<Menu.Item
+									key="logout"
+									icon={<LogoutOutlined />}
+									onClick={() => {
+										props.logout();
+									}}
+								>
+									Выход
+								</Menu.Item>
+							</Menu>
+						</div>
+					</div>
 				</Header>
 				<Content
 					className="site-layout-background"
